23a39c81782e4d17e39d1c3761fbb3367048e116
Abu Jubaer <18-38784-3@student.aiub.edu>
2021-03-02 22:13:09 +0600

    Update login.php

--- Employee/login.php
+++ Employee/login.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -5,57 +9,114 @@
     <title>Login</title>
   </head>
   <body>
+  	<center>
 
-    <h1>Employee Login</h1>
+	    <h1>Login</h1>
 
-    <?php
-      $userNameErr = $passwordErr = "" ;
+	    <?php
+			$userNameErr = $passwordErr = "" ;
 
-      $userName = "";
-      $password = "";
-      $rEmail = "";
+			$userName = "";
+			$password = "";
+			$msg = "";
+			$flag = 0;
 
-      if($_SERVER["REQUEST_METHOD"] == "POST") {
+			$filepath = "login.txt";
+			$file = fopen($filepath,'r')
+			or die("unable to open file");
 
-        if(empty($_POST['uname'])) {
-          $userNameErr = "Please fill up the username properly";
-          }
-        else {
-          $userName = $_POST['uname'];
-        }
+			if($_SERVER["REQUEST_METHOD"] == "POST") 
+			{
 
-        if(empty($_POST['password'])) {
-          $passwordErr = "Please fill up the password properly";
-        }
-        else {
-          $password = $_POST['password'];
-        }
-      }
+				if(empty($_POST['uname'])) {
+				  $userNameErr = "Please fill up the username properly";
+				  }
+				else {
+				  $userName = $_POST['uname'];
+				}
 
-    ?>
+				if(empty($_POST['password'])) {
+				  $passwordErr = "Please fill up the password properly";
+				}
+				else {
+				  $password = $_POST['password'];
+				}
 
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
-      <fieldset>
-        <legend>Login </legend>
+				while($line = fgets($file))
+				{
 
-        <label for="uname">UserName:</label>
-        <input type="text" name="uname" id="uname" value="<?php echo $userName; ?>">
-        <br>
-        <p style="color:red"><?php echo $userNameErr; ?></p>
+	                //list($firstName,$lastName,$gender,$email,$userNameV,$passwordV,$recoveryEmail) = explode( ",", $line );
 
-        <label for="pass">Password:</label>
-        <input type="password" name="password" id="password" value="<?php echo $password; ?>">
-        <br>
-        <p style="color:red"><?php echo $passwordErr; ?></p>
-       
 
-      </fieldset>
-      <br>
-      
-      <input type="submit" value="Login">
+	                $json_decoded_text = json_decode($line, true);
 
-      </form>
-      <br>
+	                $userNameV= $json_decoded_text['userName'];
+	                $passwordV= $json_decoded_text['password'];
+	        
+	                if($userNameV == $userName && $passwordV == $password)
+	                {
+	                    $flag++;
+	                    break;
+	                }       
+            	}
 
+            	if ($flag>0)
+	            {
+	                $msg = "Logged in";
+	                echo $msg;
+	                echo "<br>";
+	        
+	                $_SESSION['userNameV'] = $userName;
+	                $_SESSION['passwordV'] = $password;
+	            
+	                echo "UserName: " . $_SESSION['userNameV'];
+	                echo "<br>";
+	                echo "Password is: " . $_SESSION['passwordV'];
+	            }
+	        
+	            else
+	            {
+	                $msg = "Login Denied!!!! Try again...";
+	                echo $msg;
+	            }
+
+
+	        }
+
+	        session_unset();
+		    session_destroy();
+		    fclose($file);
+
+
+
+	    ?>
+
+	    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+	    	
+			<fieldset style="margin: 0px 700px ;">
+	        <legend>Login </legend>
+
+	        <label for="uname">UserName:</label>
+	        <input type="text" name="uname" id="uname" value="<?php echo $userName; ?>">
+	        <br>
+	        <p style="color:red"><?php echo $userNameErr; ?></p>
+
+	        <label for="pass">Password:</label>
+	        <input type="password" name="password" id="password" value="<?php echo $password; ?>">
+	        <br>
+	        <p style="color:red"><?php echo $passwordErr; ?></p>
+	       
+
+			</fieldset>
+			<br>
+
+			<input type="submit" value="Login">
+
+	    </form>
+
+	      <p style="color:red"><?php echo $userName; ?></p>
+	      <p style="color:red"><?php echo $password; ?></p>
+
+      </center>
     </body>
 </html>
