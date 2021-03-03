bcd18382b282b006a831e682bbab675420e89569
Tanvir Shahriar <tanvirsiam1101@gmail.com>
2021-03-03 20:28:24 +0600

    c

---
+++ User/Payment.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body> 
+  </html>
+  <form>
+  <fieldset>
+
+  <legend>Billing Address</legend>
+            <br>
+            
+            <label for="fname">Full Name: </label>
+            <input type="text" id="fname" name="fname">
+            <br>
+            <br>
+
+            <label for="email">Email: </label>
+            <input type="text" id="email" name="email">
+            <br>
+            <br>
+
+            <label for="address">Address: </label>
+            <input type="text" id="address" name="address">
+            <br>
+            <br>
+
+            <label for="city">City: </label>
+            <input type="text" id="city" name="city">
+            <br>
+            <br>
+
+            <label for="state">State: </label>
+            <input type="text" id="state" name="state">
+            <br>
+            <br>
+              
+            <label for="zip">Zip: </label>
+            <input type="number" id="zip" name="zip">
+            <br>
+            <br>
+
+            </fieldset>
+            <br>
+
+
+            <fieldset>
+            <legend>Payment Option</legend>
+            <br>
+            
+            <label for="cname">Owner Name: </label>
+            <input type="text" id="cname" name="cname">
+            <br>
+            <br>
+
+            <label for="cnum">Card number: </label>
+            <input type="text" id="cnum" name="cnum" placeholder="XXXX-XXXX-XXXX-XXXX">
+            <br>
+            <br>
+
+            <label for="expmonth">Exp Month: </label>
+            <input type="text" id="expmonth" name="expmonth">
+            <br>
+            <br>
+
+            <label for="expyear">Exp Year: </label>
+            <input type="number" id="expyear" name="expyear">
+            <br>
+            <br>
+
+            <label for="cvv">CVV: </label>
+            <input type="number" id="cvv" name="cvv">
+            <br>
+            <br>
+
+            <label>
+            <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
+            </label>
+            <input type="submit" value="Continue to checkout">
+            <br>
+            <br>
+          </fieldset>
+          <br>
+
+      <fieldset>
+        <legend>Oder Detail</legend>
+
+      <p><a href="#">Product 1</a> <span class="price">$15</span></p>
+      <p><a href="#">Product 2</a> <span class="price">$5</span></p>
+      <p><a href="#">Product 3</a> <span class="price">$8</span></p>
+      <p><a href="#">Product 4</a> <span class="price">$2</span></p>
+      <br>
+      <hr>
+      <p>Total <span class="price" style="color:black"><b>$30</b>
+
+</fieldset>
+</form>
+</body>
+</fieldset>
+
