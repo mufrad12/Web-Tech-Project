947730644adf0972f78dabc994523c333958dba9
Abu Jubaer <18-38784-3@student.aiub.edu>
2021-03-10 21:53:15 +0600

    Create header.php

---
+++ Employee/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="menu">
+		<div class="mid">
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="employeeManagement.php">Profile Update/Delete</a></li>
+				<li><a href="#">Salary/Festival Bonus</a>
+					<ul>
+						<li><a href="AddSalaryBonus.php">Add Salary/Festival Bonus</a></li>
+						<li><a href="ShowSalaryBonus.php">Show Salary/Festival Bonus</a></li>
+						
+					</ul>
+				</li>
+				<li><a href="#">Customer Order Information</a>
+					<ul>
+						<li><a href="orderSearch.php">Search Order</a></li>
+						<li><a href="checkorderfromUsers.php">Check Order List</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Check Transaction History</a></li>
+				<li><a href="support.php">Support</a></li>
+				<li><a href="#">Logout</a></li>
+			</ul>
+		</div>
+	</div>
+
+</body>
+</html>
