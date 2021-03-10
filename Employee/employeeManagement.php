816ccbfc74e834a14e3564e2ba78b1bbc29855ca
Abu Jubaer <18-38784-3@student.aiub.edu>
2021-03-11 00:14:41 +0600

    Update employeeManagement.php

--- Employee/employeeManagement.php
+++ Employee/employeeManagement.php
@@ -11,8 +11,8 @@
     <h1>Update and Delete Profile</h1>
 
     <?php
-        $firstNameErr = $lastNameErr = $genderErr =  $dobErr = $emailErr = $idErr = $userNameErr = $passwordErr = $conPasswordErr = "" ;
-
+       $srcAErr = $firstNameErr = $lastNameErr = $genderErr =  $dobErr = $emailErr = $idErr = $userNameErr = $passwordErr = $conPasswordErr = "" ;
+      $srcA = "";
       $firstName = ""; 
       $lastName = "";
       $gender = "";
@@ -22,88 +22,189 @@
       $userName= "";
       $password= "";
       $conPassword = "";
+      $flag = 0;
+    $searchKey = "";
 
-       if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        if(empty($_POST['fname'])) {
-          $firstNameErr = "Please fill up the first name properly";
+      if(isset($_POST['src'])){
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        if(empty($_POST['srcA'])) {
+          $srcAErr = "Please Write Your userName";
         }
         else {
-          $firstName = $_POST['fname'];
+          $srcA = $_POST['srcA'];
+          }
         }
 
-        if(empty($_POST['lname'])) {
-          $lastNameErr = "Please fill up the last name properly";
-        }
-        else {
-          $lastName = $_POST['lname'];
-        }
+      $f1 = fopen("login.txt", "r");
+      $data = fread($f1, filesize("login.txt"));
+      fclose($f1);
+      $data_after_newline_delimeter = explode("\n", $data);
+      $arr1 = array();
+      $searchKey = $srcA;
 
-        if(empty($_POST['dob'])) {
-          $dobErr = "Please fill up the date of birth properly";
+      for($i = 0; $i < count($data_after_newline_delimeter) - 1; $i++) {
+        $json_decoded = json_decode($data_after_newline_delimeter[$i], true);
+        if($json_decoded['userName'] === $searchKey)
+        {
+          echo $srcA." found";
+          $flag=1;
+          $firstName = $json_decoded['firstName']; 
+          $lastName = $json_decoded['lastName']; 
+          $email = $json_decoded['email']; 
+          $id = $json_decoded['id']; 
+          $userName= $json_decoded['userName']; 
+          $password= $json_decoded['password']; 
+          $conPassword = $json_decoded['conPassword']; 
         }
-        else {
-          $dob = $_POST['dob'];
-        }
+          }
+          if($flag==0)
+          echo $srcA." not found";
 
-        if(empty($_POST['email'])) {
-          $emailErr = "Please enter an email";
-        }
-        else {
-          $email = $_POST['email'];
+      }
+      if((isset($_POST['update']))||(isset($_POST['delete'])))
+        {
 
-          if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
-            { 
-              $emailErr = "Invalid email format"; 
-            }
-        }
+         if($_SERVER["REQUEST_METHOD"] == "POST") 
+         {
+        
+          if(empty($_POST['firstName'])) 
+          {
+        $firstNameErr = "Please fill up the first name properly";
+      }
+      else {
+        $firstName = $_POST['firstName'];
+      }
 
-        if(empty($_POST['id'])) {
+      if(empty($_POST['lastName'])) {
+        $lastNameErr = "Please fill up the last name properly";
+      }
+      else {
+        $lastName = $_POST['lastName'];
+      }
+
+            if(empty($_POST['id'])) {
         $idErr = "Please fill up the id number properly";
       }
       else {
         $id = $_POST['id'];
       }
 
-        if(empty($_POST['uname'])) {
-          $userNameErr = "Please fill up the username properly";
-          }
-        else {
-          $userName = $_POST['uname'];
+       if(empty($_POST['email'])) {
+        $emailErr = "Email is required";
+      }
+      else {
+        $email = $_POST['email'];
+      
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+        { $emailErr = "Invalid email format"; }
+             }
+
+       if(empty($_POST['userName'])) {
+        $userNameErr = "Please fill up the userName properly";
+      }
+      else {
+        $userName = $_POST['userName'];
+      }
+           if(empty($_POST['password'])) {
+        $passwordErr = "Please fill up the password properly";
+      }
+      else {
+        $password = $_POST['password'];
+      }
+           if(empty($_POST['conPassword'])) {
+        $conPasswordErr = "Please fill up the password properly";
+      }
+      else {
+        $conPassword = $_POST['conPassword'];
+      }
+      if($conPassword!==$conPassword)
+      {
+        $conPasswordErr="Password don't match";
+      }
+
+    }
+
+
+          $f1 = fopen("login.txt", "r");
+          $data = fread($f1, filesize("login.txt"));
+          fclose($f1);
+          $data_after_newline_delimeter = explode("\n", $data);
+          $arr1 = array();
+          $searchKey = $userName;
+
+        if(isset($_POST['update']))
+    {
+
+      for($i = 0; $i < count($data_after_newline_delimeter) - 1; $i++) {
+        $json_decoded = json_decode($data_after_newline_delimeter[$i], true);
+        if($json_decoded['userName'] === $searchKey)
+        {
+
+          $arr2 = array(
+            'firstName' => $firstName,
+            'lastName' => $lastName,
+            'email' => $email,
+            'id' => $json_decoded['id'],
+            'userName' => $json_decoded['userName'],
+            'password' => $password,
+            'conPassword' => $conPassword
+          );
+          array_push($arr1, $arr2);
         }
 
-        if(empty($_POST['password'])) {
-          $passwordErr = "Please fill up the password properly";
+        else
+        {
+          array_push($arr1, $json_decoded);
         }
-        else {
-          $password = $_POST['password'];
+      }
+
+      $f2 = fopen("login.txt", "w");
+      for($j = 0; $j < count($arr1); $j++) {
+        $json_encoded = json_encode($arr1[$j]);
+        fwrite($f2, $json_encoded . "\n");
+      }
+      fclose($f2);
+
+    }
+
+    if(isset($_POST['delete']))
+    {
+
+      for($i = 0; $i < count($data_after_newline_delimeter) - 1; $i++) {
+        $json_decoded = json_decode($data_after_newline_delimeter[$i], true);
+        if($json_decoded['userName'] !== $searchKey) {
+          array_push($arr1, $json_decoded);
         }
 
-        if(empty($_POST['conPassword'])) {
-        $conPasswordErr = "Recovery Email is required";
-        }
-        else {
-          $conPassword = $_POST['conPassword'];
-        
-             if (!($conPassword == $password)) 
-          { $conPasswordErr = "Password not matched"; }
-        }
+      }
+      $f2 = fopen("login.txt", "w");
+      for($j = 0; $j < count($arr1); $j++) {
+        $json_encoded = json_encode($arr1[$j]);
+        fwrite($f2, $json_encoded . "\n");
+      }
+      fclose($f2);
 
-        if (empty($_POST['gender'])) {
-               $genderErr = "Gender is required"; 
-        } 
+    }
 
-        else { 
-          $gender = $_POST['gender']; 
-        }
-        }
-
+  
+  }
     ?>
     
 
     </form>
     
+     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
 
+     <label for="srcA">User ID:</label>
+        <input type="search" name="srcA" id="srcA" value="<?php echo $srcA;?>" placeholder="Enter your user ID">
+
+        <input type="submit" name="src" value="Search" class="srcAserBtn">
+        <p style="color:red"><?php echo $srcAErr; ?></p>
+
+      </form>
+      <br>
 
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" align="center">
       <fieldset style="margin: 0px 700px ;">
@@ -179,7 +280,7 @@
 
       </form>
       <br>
-	  <div class="footer">
+    <div class="footer">
       <?php include 'footer.php';?>
       </div>
     </body>
