a37a2e31226edab1365edb08e8473ed469b9c1be
Abu Jubaer <18-38784-3@student.aiub.edu>
2021-03-11 22:44:46 +0600

    Create ShowStatement.php

---
+++ Employee/ShowStatement.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Show Salary and Festival Bonus</title>
+ </head>
+ <center>
+<body>
+
+
+<style> table, th, td 
+     {
+      border: 1px solid black;
+
+      border-collapse: collapse;
+      
+      }
+    th, td {
+      padding: 15px;
+         }
+
+         th {
+  text-align: center;
+           }
+
+           td {
+  text-align: center;
+        }
+
+</style>
+
+
+
+ 
+ <div class="header">
+      <?php include 'header.php';?>
+  </div>
+<h1>Full Income Statement</h1>
+<br><br><br>
+
+
+<?php
+
+
+
+//$a="abcd";
+
+  $f1 = fopen("statement.txt", "r");
+  $data = fread($f1, filesize("statement.txt"));
+  fclose($f1);
+
+
+
+
+
+  $data_after_newline_delimeter = explode("\n", $data);
+
+
+
+  echo '<table>
+              
+            <tr>
+                <th>Date</th>
+
+                <th>Expenditure</th>
+
+                <th>Ammount</th>
+            </tr>';
+
+
+
+
+
+
+  for($i = 0; $i < count($data_after_newline_delimeter) - 1; $i++)
+  {
+    $json_decoded = json_decode($data_after_newline_delimeter[$i], true);
+
+
+
+     echo "<tr>";
+
+      echo "<td>" . $json_decoded['date'] . "</td>";
+
+      echo "<td>" . $json_decoded['expenditure'] . "</td>";
+
+      echo "<td>" . $json_decoded['ammount'] . "</td>";
+
+      echo "</tr>";
+
+      }
+
+    echo "</table>";
+
+?>
+
+
+
+  <div class="footer">
+      <?php include 'footer.php';?>
+  </div>
+
+
+</body>
+ </center>
+</html>
