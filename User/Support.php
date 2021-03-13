41bdee32db5b2fcdc49263d356090c4a8c8f9813
Tanvir Shahriar <tanvirsiam1101@gmail.com>
2021-03-13 21:36:35 +0600

    updated

--- User/Support.php
+++ User/Support.php
@@ -5,118 +5,117 @@
     <title>Contact Us</title>
   </head>
   <body>
-
+  	
 <h1>Contact Us</h1>
 
 
 <?php
-    $nameErr = $numberErr= $subjectErr=  $emailErr="";
+		$nameErr = $numberErr= $emailErr= $subjectErr="";
 
-    $name = ""; 
-    $number = ""; 
-    $subject= "";
-    $email = "";
-
-
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-      if(empty($_POST['name'])) {
-        $nameErr = "Please fill up the name properly";
-      }
-      else {
-        $name = $_POST['name'];
-      }
-
-      if(empty($_POST['number'])) {
-        $numberErr = "Please fill up the mobile number properly";
-      }
-      else {
-        $number = $_POST['number'];
-      }
-
-       if(empty($_POST['email'])) {
-        $emailErr = "Email is required";
-      }
-      else {
-        $email = $_POST['email'];
-      
-      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
-        { $emailErr = "Invalid email format"; }
-             }
-
-       if(empty($_POST['subject'])) {
-        $subjectErr = "Please fill up the subject properly";
-      }
-      else {
-        $subject = $_POST['subject'];
-      }
-    }
-  ?>
+		$name = ""; 
+		$number = ""; 
+		$email = "";
+		$subject="";
 
 
-<table style="width:100%;max-width:550px;border:1;" cellpadding="8" cellspacing="0">
+		if($_SERVER["REQUEST_METHOD"] == "POST") {
+			if(empty($_POST['name'])) {
+				$nameErr = "Please fill up the name properly";
+			}
+			else {
+				$name = $_POST['name'];
+			}
+
+			if(empty($_POST['number'])) {
+				$numberErr = "Please fill up the mobile number properly";
+			}
+			else {
+				$number = $_POST['number'];
+			}
+
+			 if(empty($_POST['email'])) {
+				$emailErr = "Email is required";
+			}
+			else {
+				$email = $_POST['email'];
+			
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+				{ $emailErr = "Invalid email format"; }
+		         }
+
+	        if(isset($_POST['subject'])) {
+				$subjectErr = "Please fill up the comments properly";
+			}
+			else {
+				$subject = $_POST['subject'];
+			}
+
+		     }
+
+
+
+	?>
+
+
+
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
-   
-           
-  <tr> 
-    <td>
-      <label for="name">Name :</label>
 
-    </td> 
+	
 
-    <td>
-      <input type="text" id="name" name="name" style="width:250px;" value="<?php echo $name; ?>" >
-      <p style="color:red"><?php echo $nameErr; ?></p>
-    </td> 
-  </tr> 
+			<label for="name">Name :</label>
+			<input type="text" id="name" name="name" style="width:250px;margin-left: 80px;vertical-align: middle;" value="<?php echo $name; ?>" >
+			<p style="color:red"><?php echo $nameErr; ?></p>
+	
 
-  <tr> 
-    <td>
-      <label for="number">Phone number:</label>
-    </td> 
-    <td>
-      <input name="number" type="text" style="width:250px;" value="<?php echo $number; ?>" >
-      <p style="color:red"><?php echo $numberErr; ?></p>
-    </td> 
-  </tr> 
+			<label for="number">Phone number:</label>
+			<input name="number" type="text" style="width:250px;margin-left: 30px;vertical-align: middle;" value="<?php echo $number; ?>" >
+			<p style="color:red"><?php echo $numberErr; ?></p>
 
-  <tr> 
-    <td>
-      <label for="email">Email address:</label>
-    </td> 
-    <td>
-      <input name="email" type="text" style="width:250px;" value="<?php echo $email; ?>" >
-      <p style="color:red"><?php echo $emailErr; ?></p>
-    </td> 
-  </tr> 
 
-  <tr> 
-    <td>
-      <label for="subject">Comments:</label>
-    </td> 
-    <td>
-      <textarea name="subject" rows="7" cols="40" style="width:350%;max-width:550px;" value="<?php echo $subject; ?>" >
+			<label for="email">Email address:</label>
+			<input name="email" type="text" style="width:250px;margin-left: 30px;vertical-align: middle;" value="<?php echo $email; ?>" >
+			<p style="color:red"><?php echo $emailErr; ?></p>
+ 
 
-      </textarea>
+			<label for="subject">Comments:</label>
 
-  </td>
-  </tr> 
-     
-     <tr> 
-    <td>
-      
+			<textarea name="subject" rows="7" cols="40" style="width:650px;margin-left: 50px;vertical-align: middle;" 
+			value="<?php echo $subject; ?>" >
+			</textarea>
 
-    </td> 
+			<p style="color:red"><?php echo $subjectErr; ?></p>
 
-    <td>
-      <input type="submit" value="Submit">
-    </td> 
-  </tr> 
 
-  
-  </form>
-    </table>
-      
+			
+			<br>
+            <br>
+			<input type="submit" value="Submit" style="width:100px;margin-left: 125px;vertical-align: middle;">
+
+	
+	</form>
+	<br>
+      <?php
+
+         if ($name!=""  && $number!="" &&  $email!="" &&  $subject!="")
+				{ echo "<b> The form is submitted </b> ";}
+		   ?>
+   <?php
+
+      if ($name!=""  && $number!="" &&  $email!=""&&  $subject!="")
+      {
     
+        $arr1 = array('name' => $name, 'number' => $number, 'email' => $email, 
+        	'subject' => $subject
+        );
 
-    </body>
+          $json_encoded_text = json_encode($arr1); 
+            
+            $file1 = fopen("supportShop.txt", "a");
+            fwrite($file1, $json_encoded_text);
+            fwrite($file1, "\n");
+
+            fclose($file1);
+        }
+      ?>
+		</body>
 </html>
